12d66255f4556d8e3390294713435f77d0210cef
Simona Roboiu <sroboiu@endava.com>
2016-08-12 17:10:47 +0300

    request to wiki

--- Wiki-Explorer/src/main/java/com/endava/wiki/service/ArticleService.java
+++ Wiki-Explorer/src/main/java/com/endava/wiki/service/ArticleService.java
@@ -15,5 +15,6 @@ public interface ArticleService {
     ArticleDTO getArticleByName(String articleName);
     List<ArticleDTO> getAllArticles();
     ArticleDTO saveArticle(ArticleDTO articleDTO);
+    void getArticleFromWeb(String articleName);
 
 }
--- Wiki-Explorer/src/main/java/com/endava/wiki/service/impl/ArticleServiceImpl.java
+++ Wiki-Explorer/src/main/java/com/endava/wiki/service/impl/ArticleServiceImpl.java
@@ -5,13 +5,21 @@ import com.endava.wiki.entity.Article;
 import com.endava.wiki.mapping.MapEntityDTO;
 import com.endava.wiki.repository.ArticleRepository;
 import com.endava.wiki.service.ArticleService;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by sroboiu on 11-Aug-16.
@@ -22,13 +30,16 @@ public class ArticleServiceImpl implements ArticleService {
     @Autowired
     private ArticleRepository articleRepository;
 
+    @Autowired
+    private MapEntityDTO mappingArticle;
+
 
     @Override
     @Transactional(readOnly = true)
     public ArticleDTO getArticleById(Long articleId) {
         Article article = articleRepository.findOne(articleId);
         // map entity to DTO
-        return new MapEntityDTO().mapEntityToDto(article);
+        return mappingArticle.mapEntityToDto(article);
     }
 
     @Override
@@ -36,19 +47,19 @@ public class ArticleServiceImpl implements ArticleService {
     public ArticleDTO getArticleByName(String articleName) {
         Article article = articleRepository.findByArticleName(articleName);
         // map entity to DTO
-        return new MapEntityDTO().mapEntityToDto(article);
+        return mappingArticle.mapEntityToDto(article);
     }
 
     @Override
     @Transactional(readOnly = true)
     public List<ArticleDTO> getAllArticles() {
 
-        List <ArticleDTO> articles = new ArrayList<ArticleDTO>();
+        List<ArticleDTO> articles = new ArrayList<ArticleDTO>();
         List<Article> articlesEntities = articleRepository.findAll();
-        if(articlesEntities != null && !articlesEntities.isEmpty()) {
+        if (articlesEntities != null && !articlesEntities.isEmpty()) {
             for (Article article : articlesEntities) {
                 /// map entity to DTO
-                articles.add(new MapEntityDTO().mapEntityToDto(article));
+                articles.add(mappingArticle.mapEntityToDto(article));
             }
         }
         return articles;
@@ -59,5 +70,27 @@ public class ArticleServiceImpl implements ArticleService {
     public ArticleDTO saveArticle(ArticleDTO articleDTO) {
         Article article = new MapEntityDTO().mapDtoToEntity(articleDTO);
         return new MapEntityDTO().mapEntityToDto(articleRepository.save(article));
+    }
+
+    public void getArticleFromWeb(String articleName) {
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        HttpPost request = new HttpPost("https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&explaintext=&titles=" + articleName);
+        HttpResponse response = null;
+
+        try {
+            response = httpClient.execute(request);
+            String jsonStr = EntityUtils.toString(response.getEntity());
+
+            JSONObject jsonObject = new JSONObject(jsonStr);
+            JSONObject pages = jsonObject.getJSONObject("query").getJSONObject("pages");
+            String content = (String) pages.getJSONObject(pages.keys().next()).get("extract");
+
+            System.out.println("Response:\n" + content);
+            // todo: class for parsing => hashtable => articleDto (articleName, hasttable)
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
