c5185f691b985d46142ddbf6b3ff87fd042e5906
Simona Roboiu <sroboiu@endava.com>
2016-08-16 12:10:17 +0300

    parser

---
+++ Wiki-Explorer/src/main/java/com/endava/wiki/parser/ContentParser.java
@@ -0,0 +1,17 @@
+package com.endava.wiki.parser;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Hashtable;
+
+/**
+ * Created by sroboiu on 16-Aug-16.
+ */
+@Component
+public class ContentParser {
+
+    public Hashtable<String, Integer> parseContent(String content) {
+
+        return null;
+    }
+}
---
+++ Wiki-Explorer/src/main/java/com/endava/wiki/parser/FileParser.java
@@ -0,0 +1,16 @@
+package com.endava.wiki.parser;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * Created by sroboiu on 16-Aug-16.
+ */
+@Component
+public class FileParser {
+
+    public List<String> parseInputFile(String content) {
+        return null;
+    }
+}
