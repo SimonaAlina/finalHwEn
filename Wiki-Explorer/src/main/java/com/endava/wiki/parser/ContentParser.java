5aa6174acb2fc97a538189ef7450c783bd9a51f4
bobo1195 <Alexandru.Bogdan@endava.com>
2016-08-16 16:21:47 +0300

    common words delimiter

--- Wiki-Explorer/src/main/java/com/endava/wiki/parser/ContentParser.java
+++ Wiki-Explorer/src/main/java/com/endava/wiki/parser/ContentParser.java
@@ -3,12 +3,33 @@ package com.endava.wiki.parser;
 import org.springframework.stereotype.Component;
 
 import java.util.Hashtable;
+import java.util.LinkedHashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 @Component
 public class ContentParser {
 
+
     private String delimiters = " ;:/?~\\\\.,><_~`[{()}]!@#$%^&-+'=*\\\"|\t\n";
+    private static final Set<String> COMMON_WORDS = new LinkedHashSet<>();
+
+    static {
+        COMMON_WORDS.add("a");
+        COMMON_WORDS.add("and");
+        COMMON_WORDS.add("be");
+        COMMON_WORDS.add("for");
+        COMMON_WORDS.add("from");
+        COMMON_WORDS.add("has");
+        COMMON_WORDS.add("i");
+        COMMON_WORDS.add("in");
+        COMMON_WORDS.add("is");
+        COMMON_WORDS.add("it");
+        COMMON_WORDS.add("of");
+        COMMON_WORDS.add("on");
+        COMMON_WORDS.add("to");
+        COMMON_WORDS.add("the");
+    }
 
     public Hashtable<String, Integer> parseContent(String content) {
 
@@ -17,7 +38,9 @@ public class ContentParser {
         StringTokenizer token = new StringTokenizer(content, delimiters);
         while (token.hasMoreTokens()) {
             String word = token.nextToken().toLowerCase();
-            if(word.matches("[0-9]+"))
+            if (word.matches("[0-9]+"))
+                continue;
+            if (COMMON_WORDS.contains(word))
                 continue;
             if (wordsFreq.get(word) == null) {
                 wordsFreq.put(word, 1);
