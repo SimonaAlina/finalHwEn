df9dfec168d58e0a319f2ee545cc8433573fb912
bobo1195 <Alexandru.Bogdan@endava.com>
2016-08-17 11:18:10 +0300

    article sql's

--- db/article_top_words.sql
+++ db/article_top_words.sql
@@ -1,8 +1,15 @@
-CREATE TABLE IF NOT EXISTS `wikiindexer`.`articles` (
+CREATE TABLE IF NOT EXISTS `wikiindexer`.`article_top_words` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `article_name` VARCHAR(45) NULL DEFAULT NULL,
+  `word` VARCHAR(45) NULL DEFAULT NULL,
+  `count` INT(11) NULL DEFAULT NULL,
+  `art_id` INT(11) NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `article_name_UNIQUE` (`article_name` ASC))
+  UNIQUE INDEX `UNIQUE` (`word` ASC, `art_id` ASC),
+  INDEX `FK_ART_ID_idx` (`art_id` ASC),
+  CONSTRAINT `FK_ART_ID`
+    FOREIGN KEY (`art_id`)
+    REFERENCES `wikiindexer`.`articles` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB
-AUTO_INCREMENT = 1
 DEFAULT CHARACTER SET = utf8
