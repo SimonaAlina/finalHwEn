2853c18180816bf919a96b39875723828b4edeb9
bobo1195 <Alexandru.Bogdan@endava.com>
2016-08-19 17:26:14 +0300

    tests updated

--- Wiki-Explorer/tests/config/MyAppInitializerTest.java
+++ Wiki-Explorer/tests/config/MyAppInitializerTest.java
@@ -18,7 +18,7 @@ import static org.mockito.Mockito.when;
  */
 @RunWith(MockitoJUnitRunner.class)
 public class MyAppInitializerTest {
-    
+
     @Test
     public void onStartup() throws Exception {
 
